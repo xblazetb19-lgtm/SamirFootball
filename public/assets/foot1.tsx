e29525a972c3da733cbb696964bd9a49c23c717f
xblazetb19-lgtm <xblazetb19@gmail.com>
2026-03-18 15:31:20 +0100

    Add files via upload

---
+++ public/assets/foot1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="foot1" tilewidth="32" tileheight="32" tilecount="7" columns="7">
+ <image source="foot1.png" width="224" height="32"/>
+</tileset>
